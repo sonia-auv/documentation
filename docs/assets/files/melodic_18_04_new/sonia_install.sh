552370134701f428f577091ef95f38869f3e52bc
Lucas Mongrain <xeaprog@gmail.com>
2019-06-19 19:41:01 -0400

    update ros key

--- docs/assets/files/melodic_18_04_new/sonia_install.sh
+++ docs/assets/files/melodic_18_04_new/sonia_install.sh
@@ -73,7 +73,7 @@ function install_dev_environment() {
 
     if [ -z "$SONIA_WORKSPACE_ROOT" ]; then
         sudo sh -c 'echo "deb http://packages.ros.org/ros/ubuntu $(lsb_release -sc) main" > /etc/apt/sources.list.d/ros-latest.list'
-        sudo apt-key adv --keyserver hkp://ha.pool.sks-keyservers.net:80 --recv-key 421C365BD9FF1F717815A3895523BAEEB01FA116
+        sudo apt-key adv --keyserver hkp://ha.pool.sks-keyservers.net:80 --recv-key C1CF6E31E6BADE8868B172B4F42ED6FBAB17C654
         
         sudo apt update -y && sudo apt dist-upgrade -y
         sudo apt install -y -f make \
@@ -140,7 +140,7 @@ function install_jetson_agx_environment() {
     if [ -z "$SONIA_WORKSPACE_ROOT" ]; then
         ## Add source to ROS melodic
         sudo sh -c 'echo "deb http://packages.ros.org/ros/ubuntu $(lsb_release -sc) main" > /etc/apt/sources.list.d/ros-latest.list'
-        sudo apt-key adv --keyserver hkp://ha.pool.sks-keyservers.net:80 --recv-key 421C365BD9FF1F717815A3895523BAEEB01FA116
+        sudo apt-key adv --keyserver hkp://ha.pool.sks-keyservers.net:80 --recv-key C1CF6E31E6BADE8868B172B4F42ED6FBAB17C654
 
         #Install basic system dependcies and upgrades
         sudo apt update -y && sudo apt upgrade -y
